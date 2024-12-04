dc3ca51c67dba34a627ad561e41c64497697511f
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-12-04 09:51:51 +0800

    修改檔案路徑

--- js/loadpage.js
+++ js/loadpage.js
@@ -1,16 +1,16 @@
-fetch('header-aside.html')
+fetch('../header-aside.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('layout-menu').innerHTML = data;
       })
       .catch(error => console.error('Error loading header:', error));
-fetch('header-nav.html')
+fetch('../header-nav.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('layout-navbar').innerHTML = data;
       })
       .catch(error => console.error('Error loading header:', error));
-fetch('footer-alert.html')
+fetch('../footer-alert.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('buynow').innerHTML = data;
