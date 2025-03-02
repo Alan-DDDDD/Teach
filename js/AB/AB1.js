7175239e75d26f97eddb7755ac434d8ef7e5fbd6
alan <alan20100525@gmail.com>
2025-03-02 20:28:51 +0800

    AB1

---
+++ js/AB/AB1.js
@@ -0,0 +1,111 @@
+export class AB1 extends baseObject {
+    constructor() {
+        super();
+    }
+
+    //初始化UB1
+    Init(){
+        let me = this;
+        me.defaultHideArea = ["L","E"];//設定預設隱藏區域
+        me.defaultToolBarDisabled = ["save","report"]//設定ToolBar按鈕狀態，預設全開
+        me.ClassName = "AB1"
+        me.InitL();//初始化L區
+        $(`#search`).on("click",me.Search.bind(this));
+        $(`#save`).on("click",me.Save.bind(this));
+        $(`#insert`).on("click",me.Insert.bind(this));
+        $(`#datatable`).on("click",".data_detaile",me.DataDetail);
+        $(`#Review`).attr("disabled","disabled");
+    }
+    InitL(){
+        //設定L區DataTable
+        let columns = [
+            { 
+                data: null,title:`<input class="form-check-input all_checkbox" type="checkbox"/>選取`,
+                orderable: false,
+                render: function(data,type,row){
+                    let html = `<input class="form-check-input list_checkbox" type="checkbox" `;
+                    html += data == 'Y' ? "checked":"";
+                    html += `/>`
+                    return html;
+                }
+            },
+            { data: 'Name',title: "訂單編號" },
+            { data: 'Contact',title: "客戶名稱" },
+            { data: 'ConfirmDt',title: "簽約日" },
+            { data: 'Salesid',title: "負責業務" },
+            { 
+                data: null,title:"操作功能",orderable: false,
+                render:function(data,type,row){
+                    let html = `<button class="btn btn-primary data_detaile" style="padding:0.125rem 0.375rem">明細</button>`
+                    return html
+                }
+            }
+        ]
+        let columnDefs = [
+            {width: '8%',targets:[0],responsivePriority:1},
+            {width: '10%',targets:[1],responsivePriority:3},
+            {width: '10%',targets:[2],responsivePriority:4},
+            {width: '10%',targets:[3],responsivePriority:5},
+            {width: '15%',targets:[4],responsivePriority:6},
+            {width: '10%',targets:[5],responsivePriority:2},
+        ]
+        let buttons = [
+            { extend: 'excel', className: 'excelButton btn-primary disabled' },
+            { extend: 'print', className: 'printButton btn-primary disabled' }
+        ]
+        this.setTable(`datatable`,[],columns,columnDefs,buttons,function(e){
+        });
+    }
+    remove(){
+        super.Deconstructor();
+        $(`#search`).unbind("click");
+        $(`#save`).unbind("click");
+        $(`#insert`).unbind("click");
+    }
+    async Save(){
+        if(super.verification("EArea")){
+            pageaction.showLoading();
+            let E = this.GetAreaData("EArea")
+            let Data = await t_Post("AA1/Save",this.ClassName,E);
+            if(Data.Status){
+                this.alertMsg("儲存成功","Success")
+                let table = $(`#datatable`).DataTable();
+                let tableData = table.rows().data().toArray();
+                tableData = tableData.map(item => 
+                    item.Comid === Data.Data.Comid ? { ...item, ...Data.Data } : item
+                );
+                this.BindDataList("datatable",tableData);
+                pageaction.hideLoading();
+            }else{
+                this.alertMsg(Data.Msg,"danger")
+            }
+        }else{
+            this.alertMsg("請輸入必填資料","danger")
+        }
+    }
+    Insert(){
+        this.ClearArea("EArea");//清空E區資料
+        pageaction.ToolBarUnDisabled("save");//解鎖save按鈕
+        pageaction.areahide("Q");//隱藏Q區
+        pageaction.areahide("L");//隱藏L區
+        pageaction.areashow("E");//展開E區
+    }
+
+    DataDetail(){
+        let table = $(`#datatable`).DataTable();
+        var rowData = table.rows($(this).parent()[0]).data()[0];
+        if(rowData){   
+            pageaction.areahide("L");
+            pageaction.areashow("E");
+            pageaction.ToolBarUnDisabled("save");
+            currentview.BindDataForArea(rowData,"EArea");
+        }
+    }
+    SetDataValid(){
+        let me = this;
+        // me.DataValid('Comid','input',/[^A-Z0-9]/g);
+        // me.DataValid('Account','input',/[^A-Za-z0-9]/g);
+        // me.DataValid('Password','input',/[^A-Za-z0-9]/g);
+    }
+}
+currentview = new AB1();
